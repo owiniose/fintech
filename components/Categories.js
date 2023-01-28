f25c61f9cab17232b8ff4c3dc7f691c299662aee
owiniose <owiniose@gmail.com>
2023-01-28 20:51:25 +0100

    commit

--- components/Categories.js
+++ components/Categories.js
@@ -13,13 +13,47 @@ const Categories = () => {
         fontSize: 16
       }}>Categories</Text>
 
-      <View>
-        <View>
-          <Text>Product Designer</Text>
+      <View style={{
+        backgroundColor:"#FFE8D2",
+        marginVertical:16,
+        borderRadius:15
+      }} >
+        <View style={{
+          paddingTop:16,
+          paddingBottom:4,
+          marginLeft:16
+        }} >
+          <Text style={{
+            fontFamily:"RedHatMedium",
+            fontSize: 16,
+          }}>
+            Product Designer</Text>
           <Image/>
         </View>
-        <Text>Facebook, Airbnb, Spotify, 72 more</Text>
-        <Text>152 </Text>
+        <Text style={{
+          marginLeft:16,
+          marginBottom:8
+        }} >Facebook, Airbnb, Spotify + 72 more</Text>
+        <View style={{
+          flexDirection: "row",
+          alignItems:"center",
+          marginLeft:16,
+          marginBottom:12,
+        }} >
+        <Text style={{
+          
+          fontFamily:"RedHatMedium",
+          fontSize:24,
+        }} >152 </Text>
+        <Text style={{
+          fontFamily:"RedHatMedium",
+          fontSize:12,
+          alignSelf:"flex-end",
+          paddingBottom:3
+        }} >
+          positions
+        </Text>
+        </View>
       </View>
 
     </View>
