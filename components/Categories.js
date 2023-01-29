4877c5af134498a1258182afac1c347c33141df6
owiniose <owiniose@gmail.com>
2023-01-29 17:27:57 +0100

    commit

--- components/Categories.js
+++ components/Categories.js
@@ -1,34 +1,42 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { AntDesign } from '@expo/vector-icons';
 
 const Categories = () => {
   return (
     <View style={{
       marginLeft: 24,
       marginTop: 16,
-      marginBottom:8
+      marginBottom:8,
     }}>
       <Text style={{
         fontFamily: "RedHatMedium",
-        fontSize: 16
+        fontSize: 16,
       }}>Categories</Text>
 
       <View style={{
+        flexDirection:"row",
+      }} >
+      <View style={{
         backgroundColor:"#FFE8D2",
         marginVertical:16,
-        borderRadius:15
-      }} >
+        borderRadius:15,
+        marginRight: 16,
+        paddingRight: 16
+        }} >
         <View style={{
           paddingTop:16,
           paddingBottom:4,
-          marginLeft:16
+          marginLeft:16,
+          flexDirection: "row",
+          justifyContent: 'space-between'
         }} >
           <Text style={{
             fontFamily:"RedHatMedium",
             fontSize: 16,
           }}>
             Product Designer</Text>
-          <Image/>
+            <AntDesign name="CodeSandbox" size={16} color="black" />
         </View>
         <Text style={{
           marginLeft:16,
@@ -55,7 +63,49 @@ const Categories = () => {
         </Text>
         </View>
       </View>
-
+      <View style={{
+        backgroundColor:"#DAE2FE",
+        marginVertical:16,
+        borderRadius:15,
+        }} >
+        <View style={{
+          paddingTop:16,
+          paddingBottom:4,
+          marginLeft:16
+        }} >
+          <Text style={{
+            fontFamily:"RedHatMedium",
+            fontSize: 16,
+          }}>
+            UI/UX Design</Text>
+          <Image/>
+        </View>
+        <Text style={{
+          marginLeft:16,
+          marginBottom:8
+        }} >Stripe, Webflow, Spotify + 72 more</Text>
+        <View style={{
+          flexDirection: "row",
+          alignItems:"center",
+          marginLeft:16,
+          marginBottom:12,
+        }} >
+        <Text style={{
+          
+          fontFamily:"RedHatMedium",
+          fontSize:24,
+        }} >95 </Text>
+        <Text style={{
+          fontFamily:"RedHatMedium",
+          fontSize:12,
+          alignSelf:"flex-end",
+          paddingBottom:3
+        }} >
+          positions
+        </Text>
+        </View>
+      </View>
+      </View>
     </View>
   )
 }
